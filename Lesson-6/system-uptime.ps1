c8ac575526f27f1aecf0acbc51194242e5a9bac2
raverav <ranik.vel@gmail.com>
2025-07-24 20:17:25 +0300

    Add files via upload

---
+++ Lesson-6/system-uptime.ps1
@@ -0,0 +1,7 @@
+﻿# Get System Uptime
+$lastBootTime = (Get-CimInstance Win32_OperatingSystem).LastBootUpTime
+$currentTime = Get-Date
+$uptime = $currentTime - $lastBootTime
+
+Write-Host "Last Boot Up Time: $lastBootTime"
+Write-Host "System Uptime: $($uptime.Days) days, $($uptime.Hours) hours, $($uptime.Minutes) minutes"
