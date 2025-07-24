6dfe3c343def04240a3344a0673e342d99744d43
raverav <ranik.vel@gmail.com>
2025-07-24 20:29:36 +0300

    ListHighRamUsage.ps1

--- Lesson-6/Wan-IP.ps1
+++
@@ -1,2 +0,0 @@
-﻿# List Processes with High Memory Usage
-Get-Process | Sort-Object -Property WS -Descending | Select-Object -First 10 ProcessName, @{Name="Memory (MB)"; Expression={$_.WS / 1MB -as [int]}}, Id | Format-Table -AutoSize
